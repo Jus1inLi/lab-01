2b37d746d1f0a1ba98fb082f68cd0f8117ae449f
1234-justin <63370768+1234-justin@users.noreply.github.com>
2026-09-01 18:51:24 -0600

    intial stage done

---
+++ PetShop/app/src/main/java/com/example/PetShop/Cat.kt
@@ -0,0 +1,11 @@
+package com.example.PetShop
+
+class Cat(name: String, age: Int): Pet(name, age), Pettable {
+    override fun speak(): String {
+        return "Meow"
+    }
+
+    override fun pet() {
+        println("The dog $name is being petted")
+    }
+}
---
+++ PetShop/app/src/main/java/com/example/PetShop/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.PetShop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val cat = Cat("Coco", 6)
+        val dog = Dog("Coco2", 2)
+        val scorpion = Scorpion("Coco3", 1)
+
+        val pets = mutableListOf<Pet>()
+        pets.add(cat)
+        pets.add(dog)
+        pets.add(scorpion)
+
+        val pettablePets = mutableListOf<Pettable>()
+            pettablePets.add(cat)
+            pettablePets.add(dog)
+            //pettablePets.add(scorpion)
+
+
+    }
+}
